1e95a195f10ab4ec26b2902a62bc71dc813b2f03
Angela Lyman <lyman.a@live.unic.ac.cy>
2023-03-03 10:32:37 +0200

    Initial Commit

---
+++ Lab1_b/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab1_b
+//
+//  Created by Angela Lyman on 03/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
